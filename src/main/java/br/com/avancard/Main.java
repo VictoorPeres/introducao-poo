fc9e7608f9387fae59c60be46a7033772ff6b40a
Victor Oliveir <alexanvictor@gmail.com>
2024-09-11 10:57:13 -0300

    Instaciação da classe aluno

--- src/main/java/br/com/avancard/Main.java
+++ src/main/java/br/com/avancard/Main.java
@@ -1,17 +1,33 @@
 package br.com.avancard;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.avancard.classes.Aluno;
+
+/* Main é um metodo auto executavel em java */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /* new Aluno() é uma instancia (Criação de objeto)*/
+        /* aluno1 é uma referencia para o objeto aluno */
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("João da Silva");
+        aluno1.setIdade(50);
+        aluno1.setDataNascimento("18/10/1974");
+        aluno1.setRegistroGeral("445.544.545-45");
+        aluno1.setNumeroCpf("445.544.545-45");
+        aluno1.setNomeMae("Shirlei");
+        aluno1.setNomePai("Antonio");
+        aluno1.setDataMatricula("10/01/2019");
+        aluno1.setSerieMatriculado("5");
+        aluno1.setNomeEscola("Escola JDEV Treinamento");
+
+        System.out.println(aluno1.toString());
+
+        Aluno aluno2 = new Aluno("Maria");
+        Aluno aluno3 = new Aluno("Meire", 58, "13/01/1966");
+
+        System.out.println(aluno2.toString());
+        System.out.println(aluno3.toString());
+
     }
 }
