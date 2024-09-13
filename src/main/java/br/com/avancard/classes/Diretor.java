51fbdee76852d8a48ac4be5016c46336940fd703
Victor Oliveir <alexanvictor@gmail.com>
2024-09-13 09:20:19 -0400

    Criação da classe DIRETOR

---
+++ src/main/java/br/com/avancard/classes/Diretor.java
@@ -0,0 +1,63 @@
+package br.com.avancard.classes;
+
+import java.util.Objects;
+
+public class Diretor extends Pessoa {
+    private String registroEducacao;
+    private String tempoDirecao;
+    private String titulacao;
+
+    public String getRegistroEducacao() {
+        return registroEducacao;
+    }
+
+    public void setRegistroEducacao(String registroEducacao) {
+        this.registroEducacao = registroEducacao;
+    }
+
+    public String getTempoDirecao() {
+        return tempoDirecao;
+    }
+
+    public void setTempoDirecao(String tempoDirecao) {
+        this.tempoDirecao = tempoDirecao;
+    }
+
+    public String getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Diretor diretor = (Diretor) o;
+        return Objects.equals(registroEducacao, diretor.registroEducacao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), registroEducacao);
+    }
+
+    @Override
+    public String toString() {
+        return "Diretor{" +
+                "registroEducacao='" + registroEducacao + '\'' +
+                ", tempoDirecao='" + tempoDirecao + '\'' +
+                ", titulacao='" + titulacao + '\'' +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                ", registroGeral='" + registroGeral + '\'' +
+                ", numeroCpf='" + numeroCpf + '\'' +
+                ", nomeMae='" + nomeMae + '\'' +
+                ", nomePai='" + nomePai + '\'' +
+                '}';
+    }
+}
